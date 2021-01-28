ad73aad32dc7396e457d86bfeca9d25f4dfdd6e7
Rocco <roccojelso77@gmail.com>
2021-01-28 05:09:48 -0500

    Adventure Game Start
    Added the basic First Person movement mechanics as well as the point and click mechanics we learned in class. Still blocking out the level and trying to figure out my assets list.

---
+++ Assets/Scripts/RayCaster.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+
+public class RayCaster : MonoBehaviour
+{
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // did the user click on this game tick?
+        if (cam != null && Input.GetButtonDown("Fire1"))
+        {
+
+            // shoot a ray into the scene
+
+            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // raycast hit a collider 
+
+                DoorController door = hit.transform.GetComponentInParent<DoorController>();
+                if (door != null) door.PlayerInteract(transform.parent.position);
+
+                ItemPickup pickup = hit.transform.GetComponent<ItemPickup>();
+                if (pickup != null) pickup.PlayerInteract();
+
+            }
+        }
+    }
+}
