566692c0e74efa9f03f9ea8c34af7c3696248a40
Rocco <roccojelso77@gmail.com>
2021-02-03 09:52:34 -0500

    Build 2.0
    The game now has pop up text boxes that can help guide the player.

---
+++ Assets/Scripts/ShowText.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowText : MonoBehaviour
+{
+    public GameObject uiObject;
+
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    void OnTriggerEnter (Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(uiObject);
+        Destroy(gameObject);
+    }
+}
